4e11fb2bc3a70f8e8c1af2f9ccccc33a4612748e
Peter Hurford <peter@peterhurford.com>
2020-10-01 21:56:44 -0500

    Move more parts of forecast into Jupyter notebook

--- forecast.py
+++ forecast.py
@@ -1,3 +1,4 @@
+import pickle
 import random
 
 import numpy as np
@@ -5,14 +6,17 @@ import pandas as pd
 
 from scipy import stats
 from pprint import pprint
+from functools import partial
+from collections import defaultdict
 
 
 N_SCENARIOS = 40000
 
-CREDIBLE_INTERVAL = 0.7  # 70% chance of donation falling within range, conditional on giving
+CREDIBLE_INTERVAL = 0.8  # 80% chance of donation falling within range, conditional on giving
 # TODO: Should we jitter the chance of giving as well, to account for correlated errors / make the interval wider?
 
 VERBOSE = False          # Change this to True for scenario-specific output
+SCENARIO_RANGES = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99]
 
 
 def parse_currency(currency):
@@ -73,42 +77,22 @@ for index, row in raw_data.iterrows():
         y2020_low = parse_currency(row[' 2020 Gift Potential - Low '])
         y2020_high = parse_currency(row[' 2020 Gift Potential - High '])
         y2020_prob = parse_percent(row['2020 Likelihood of Gift'])
-        y2020_logmean = np.mean([lognormal_sample(y2020_low, y2020_high, CREDIBLE_INTERVAL) for _ in range(int(N_SCENARIOS / 100))])
-        y2020_naive_ev = y2020_logmean * y2020_prob
         y2021_low = parse_currency(row[' 2021 Gift Potential - Low '])
         y2021_high = parse_currency(row[' 2021 Gift Potential - High '])
         y2021_prob = parse_percent(row['2021 Likelihood of Gift'])
-        y2021_logmean = np.mean([lognormal_sample(y2021_low, y2021_high, CREDIBLE_INTERVAL) for _ in range(int(N_SCENARIOS / 100))])
-
-        y2021_naive_ev = y2021_logmean * y2021_prob
 
         fundraising_data[donor] = {'2020': {'low': y2020_low,
                                             'high': y2020_high,
-                                            'logmean': y2020_logmean,
-                                            'prob': y2020_prob,
-                                            'naive_ev': y2020_naive_ev},
+                                            'prob': y2020_prob},
                                    '2021': {'low': y2021_low,
                                             'high': y2021_high,
-                                            'logmean': y2021_logmean,
-                                            'prob': y2021_prob,
-                                            'naive_ev': y2021_naive_ev}}
-
-print('-')
-print('### FUNDRAISING DATA ###')
-pprint(sorted(list(fundraising_data.items())))
-print('-')
-print('-')
+                                            'prob': y2021_prob}}
 
 y2020_all_scenario_totals = []
 y2021_all_scenario_totals = []
-import pdb
-pdb.set_trace()
-
-
-pprint([(y[0], y[1]['2021']['naive_ev']) for y in sorted(fundraising_data.items(), key=lambda x: x[1]['2021']['naive_ev'], reverse=True)])
-import pdb
-pdb.set_trace()
-
+y2020_fundraising_totals = defaultdict(partial(np.array, 0))
+y2021_fundraising_totals = defaultdict(partial(np.array, 0))
+joint_fundraising_totals = defaultdict(partial(np.array, 0))
 
 for s in range(N_SCENARIOS):
     if s % 100 == 0:
@@ -117,8 +101,9 @@ for s in range(N_SCENARIOS):
             print('### SCENARIO {} ###'.format(s + 1))
         else:
             print('... Completed {}/{}'.format(s + 1, N_SCENARIOS))
-    y2020_fundraising_totals = {}
-    y2021_fundraising_totals = {}
+
+    y2020_donations = []
+    y2021_donations = []
 
     for donor, donation in fundraising_data.items():
         if random.random() <= donation['2020']['prob']:
@@ -142,11 +127,14 @@ for s in range(N_SCENARIOS):
             print('{} gives {} in 2020 and {} in 2021'.format(donor,
                                                               print_money(y2020_donation),
                                                               print_money(y2021_donation)))
-        y2020_fundraising_totals[donor] = y2020_donation
-        y2021_fundraising_totals[donor] = y2021_donation
+        y2020_donations.append(y2020_donation)
+        y2021_donations.append(y2021_donation)
+        y2020_fundraising_totals[donor] = np.append(y2020_fundraising_totals[donor], y2020_donation)
+        y2021_fundraising_totals[donor] = np.append(y2021_fundraising_totals[donor], y2021_donation)
+        joint_fundraising_totals[donor] = np.append(joint_fundraising_totals[donor], y2020_donation + y2021_donation)
 
-    y2020_total_raised = sum(y2020_fundraising_totals.values())
-    y2021_total_raised = sum(y2021_fundraising_totals.values())
+    y2020_total_raised = sum(y2020_donations)
+    y2021_total_raised = sum(y2021_donations)
     if s % 100 == 0 and VERBOSE:
         print('TOTAL RAISED IN 2020: {}'.format(print_money(y2020_total_raised)))
         print('TOTAL RAISED IN 2021: {}'.format(print_money(y2021_total_raised)))
@@ -158,32 +146,28 @@ if VERBOSE:
     print('-')
     print('-')
 joint_scenarios = np.array(y2020_all_scenario_totals) + np.array(y2021_all_scenario_totals)
-y2020_percentiles = np.percentile(y2020_all_scenario_totals, [10, 20, 50, 80, 90])
-y2021_percentiles = np.percentile(y2021_all_scenario_totals, [10, 20, 50, 80, 90])
-joint_percentiles = np.percentile(joint_scenarios, [10, 20, 50, 80, 90])
+y2020_percentiles = np.percentile(y2020_all_scenario_totals, SCENARIO_RANGES)
+y2021_percentiles = np.percentile(y2021_all_scenario_totals, SCENARIO_RANGES)
+joint_percentiles = np.percentile(joint_scenarios, SCENARIO_RANGES)
 
-y2020_percentiles = [print_money(x) for x in y2020_percentiles]
-print('SCENARIO 2020 -- 10% {} - 20% {} - 50% {} - 80% {} - 90% {}'.format(*y2020_percentiles))
+print('SCENARIO 2020 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(y2020_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
+print('SCENARIO 2021 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(y2021_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
+print('SCENARIO -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(joint_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
 
-y2021_percentiles = [print_money(x) for x in y2021_percentiles]
-print('SCENARIO 2021 -- 10% {} - 20% {} - 50% {} - 80% {} - 90% {}'.format(*y2021_percentiles))
-
-joint_percentiles = [print_money(x) for x in joint_percentiles]
-print('SCENARIO 2020+2021 -- 10% {} - 20% {} - 50% {} - 80% {} - 90% {}'.format(*joint_percentiles))
-
-print('-')
-print('-')
-print('-')
-print('## 2020 Detailed Breakdown ##')
-print(list(zip([100 - z for z in range(100)],
-               [print_money(y) for y in np.percentile(y2020_all_scenario_totals, range(100))])))
-
-print('-')
-print('-')
-print('-')
-print('## 2020-2021 Detailed Breakdown ##')
-print(list(zip([100 - z for z in range(100)],
-               [print_money(y) for y in np.percentile(joint_scenarios, range(100))])))
+print('... Saving 1/7')
+pickle.dump(fundraising_data, open('fundraising_data.p', 'wb'))
+print('... Saving 2/7')
+pickle.dump(y2020_fundraising_totals, open('y2020_fundraising_totals.p', 'wb'))
+print('... Saving 3/7')
+pickle.dump(y2021_fundraising_totals, open('y2021_fundraising_totals.p', 'wb'))
+print('... Saving 4/7')
+pickle.dump(joint_fundraising_totals, open('joint_fundraising_totals.p', 'wb'))
+print('... Saving 5/7')
+pickle.dump(y2020_all_scenario_totals, open('y2020_all_scenario_totals.p', 'wb'))
+print('... Saving 6/7')
+pickle.dump(y2021_all_scenario_totals, open('y2021_all_scenario_totals.p', 'wb'))
+print('... Saving 7/7')
+pickle.dump(joint_scenarios, open('joint_scenarios.p', 'wb'))
 
 import pdb
 pdb.set_trace()
