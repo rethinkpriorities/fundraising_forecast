f269cff5c6492e954d266c4b7ba04229907f9691
Peter Hurford <peter@peterhurford.com>
2020-10-02 16:33:26 -0500

    Add absolute error, path, and other features

--- forecast.py
+++ forecast.py
@@ -10,13 +10,17 @@ from functools import partial
 from collections import defaultdict
 
 
-N_SCENARIOS = 40000
+N_SCENARIOS = 40000      # How many Monte Carlo simulations to run?
 
 CREDIBLE_INTERVAL = 0.8  # 80% chance of donation falling within range, conditional on giving
+ABSOLUTE_ERROR = 152250  # This is the width of the 80% normal interval in $ that we apply to every scenario (e.g., for uncertainty around the budget) (can be set to $0 to ignore)
 # TODO: Should we jitter the chance of giving as well, to account for correlated errors / make the interval wider?
 
-VERBOSE = False          # Change this to True for scenario-specific output
-SCENARIO_RANGES = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99]
+SCENARIO_RANGES = [1, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 99]  # Generate printed output for these percentiles
+VERBOSE = False                                                       # Change this to True for scenario-specific outputs
+
+SAVE = True   # Set to False to not save (overwrite) any model outputs
+PATH = ""     # Use this to define a custom path for the saved model outputs
 
 
 def parse_currency(currency):
@@ -30,7 +34,10 @@ def parse_currency(currency):
 
 
 def print_money(money):
-    return '${:,.2f}'.format(money)
+    if money >= 0:
+        return '${:,.2f}'.format(money)
+    else:
+        return '-${:,.2f}'.format(-money)
 
 
 # Either round to nearest hundred if <1000 or nearest thousand if >= 1000
@@ -47,6 +54,19 @@ def parse_percent(percent):
         return float(percent.replace('%', '')) / 100
     else:
         return 0
+
+
+def normal_sample(low, high, interval):
+    if (low > high) or (high < low):
+        raise ValueError
+    if low == high:
+        return low
+    else:
+        mu = (high + low) / 2
+        cdf_value = 0.5 + 0.5 * interval
+        normed_sigma = stats.norm.ppf(cdf_value)
+        sigma = (high - mu) / normed_sigma
+        return np.random.normal(mu, sigma)
 
 
 def lognormal_sample(low, high, interval):
@@ -134,10 +154,22 @@ for s in range(N_SCENARIOS):
         joint_fundraising_totals[donor] = np.append(joint_fundraising_totals[donor], y2020_donation + y2021_donation)
 
     y2020_total_raised = sum(y2020_donations)
-    y2021_total_raised = sum(y2021_donations)
+    if ABSOLUTE_ERROR > 0:
+        scenario_absolute_error = normal_sample(low=0.1,
+                                                high=ABSOLUTE_ERROR/2.0,
+                                                interval=CREDIBLE_INTERVAL)
+        if random.random() <= 0.5:
+            scenario_absolute_error = -scenario_absolute_error
+    else:
+        scenario_absolute_error = 0
+
+    y2021_total_raised = sum(y2021_donations) + scenario_absolute_error
+
     if s % 100 == 0 and VERBOSE:
+        print('ABSOLUTE ERROR TERM: {}'.format(print_money(scenario_absolute_error)))
         print('TOTAL RAISED IN 2020: {}'.format(print_money(y2020_total_raised)))
         print('TOTAL RAISED IN 2021: {}'.format(print_money(y2021_total_raised)))
+
     y2020_all_scenario_totals.append(y2020_total_raised)
     y2021_all_scenario_totals.append(y2021_total_raised)
 
@@ -145,6 +177,8 @@ if VERBOSE:
     print('-')
     print('-')
     print('-')
+
+
 joint_scenarios = np.array(y2020_all_scenario_totals) + np.array(y2021_all_scenario_totals)
 y2020_percentiles = np.percentile(y2020_all_scenario_totals, SCENARIO_RANGES)
 y2021_percentiles = np.percentile(y2021_all_scenario_totals, SCENARIO_RANGES)
@@ -154,20 +188,19 @@ print('SCENARIO 2020 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(
 print('SCENARIO 2021 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(y2021_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
 print('SCENARIO -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(joint_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
 
-print('... Saving 1/7')
-pickle.dump(fundraising_data, open('fundraising_data.p', 'wb'))
-print('... Saving 2/7')
-pickle.dump(y2020_fundraising_totals, open('y2020_fundraising_totals.p', 'wb'))
-print('... Saving 3/7')
-pickle.dump(y2021_fundraising_totals, open('y2021_fundraising_totals.p', 'wb'))
-print('... Saving 4/7')
-pickle.dump(joint_fundraising_totals, open('joint_fundraising_totals.p', 'wb'))
-print('... Saving 5/7')
-pickle.dump(y2020_all_scenario_totals, open('y2020_all_scenario_totals.p', 'wb'))
-print('... Saving 6/7')
-pickle.dump(y2021_all_scenario_totals, open('y2021_all_scenario_totals.p', 'wb'))
-print('... Saving 7/7')
-pickle.dump(joint_scenarios, open('joint_scenarios.p', 'wb'))
+if SAVE:
+    print('... Saving 1/7')
+    pickle.dump(fundraising_data, open('{}fundraising_data.p'.format(PATH), 'wb'))
+    print('... Saving 2/7')
+    pickle.dump(y2020_fundraising_totals, open('{}y2020_fundraising_totals.p'.format(PATH), 'wb'))
+    print('... Saving 3/7')
+    pickle.dump(y2021_fundraising_totals, open('{}y2021_fundraising_totals.p'.format(PATH), 'wb'))
+    print('... Saving 4/7')
+    pickle.dump(joint_fundraising_totals, open('{}joint_fundraising_totals.p'.format(PATH), 'wb'))
+    print('... Saving 5/7')
+    pickle.dump(y2020_all_scenario_totals, open('{}y2020_all_scenario_totals.p'.format(PATH), 'wb'))
+    print('... Saving 6/7')
+    pickle.dump(y2021_all_scenario_totals, open('{}y2021_all_scenario_totals.p'.format(PATH), 'wb'))
+    print('... Saving 7/7')
+    pickle.dump(joint_scenarios, open('{}joint_scenarios.p'.format(PATH), 'wb'))
 
-import pdb
-pdb.set_trace()
