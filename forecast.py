3fee37fb50277e519c9f73c646d39249d95cce15
Peter Hurford <peter@peterhurford.com>
2021-01-26 22:18:41 -0600

    2020 / 2021 -> 2021 / 2022

--- forecast.py
+++ forecast.py
@@ -107,32 +107,32 @@ def lognormal_sample(low, high, interval):
 
 
 raw_data = pd.read_csv(CSV)
-raw_data = raw_data[['Donor', '2020 Gift Potential - Low', '2020 Gift Potential - High',
-                     '2020 Likelihood of Gift', '2021 Gift Potential - Low',
-                     '2021 Gift Potential - High', '2021 Likelihood of Gift']]
+raw_data = raw_data[['Donor', '2021 Gift Potential - Low', '2021 Gift Potential - High',
+                     '2021 Likelihood of Gift', '2022 Gift Potential - Low',
+                     '2022 Gift Potential - High', '2022 Likelihood of Gift']]
 raw_data = raw_data[raw_data['Donor'] != 'Current Donors'][raw_data['Donor'] != 'Possible Donors']
 fundraising_data = {}
 for index, row in raw_data.iterrows():
     donor = row['Donor']
     if donor and isinstance(donor, str):
-        y2020_low = parse_currency(row['2020 Gift Potential - Low'])
-        y2020_high = parse_currency(row['2020 Gift Potential - High'])
-        y2020_prob = parse_percent(row['2020 Likelihood of Gift'])
         y2021_low = parse_currency(row['2021 Gift Potential - Low'])
         y2021_high = parse_currency(row['2021 Gift Potential - High'])
         y2021_prob = parse_percent(row['2021 Likelihood of Gift'])
+        y2022_low = parse_currency(row['2022 Gift Potential - Low'])
+        y2022_high = parse_currency(row['2022 Gift Potential - High'])
+        y2022_prob = parse_percent(row['2022 Likelihood of Gift'])
 
-        fundraising_data[donor] = {'2020': {'low': y2020_low,
-                                            'high': y2020_high,
-                                            'prob': y2020_prob},
-                                   '2021': {'low': y2021_low,
+        fundraising_data[donor] = {'2021': {'low': y2021_low,
                                             'high': y2021_high,
-                                            'prob': y2021_prob}}
+                                            'prob': y2021_prob},
+                                   '2022': {'low': y2022_low,
+                                            'high': y2022_high,
+                                            'prob': y2022_prob}}
 
-y2020_all_scenario_totals = []
 y2021_all_scenario_totals = []
-y2020_fundraising_totals = defaultdict(partial(np.array, 0))
+y2022_all_scenario_totals = []
 y2021_fundraising_totals = defaultdict(partial(np.array, 0))
+y2022_fundraising_totals = defaultdict(partial(np.array, 0))
 joint_fundraising_totals = defaultdict(partial(np.array, 0))
 constant_errors = []
 
@@ -144,17 +144,10 @@ for s in range(N_SCENARIOS):
         else:
             print('... Completed {}/{}'.format(s + 1, N_SCENARIOS))
 
-    y2020_donations = []
     y2021_donations = []
+    y2022_donations = []
 
     for donor, donation in fundraising_data.items():
-        if random.random() <= donation['2020']['prob']:
-            y2020_donation = lognormal_sample(low=donation['2020']['low'],
-                                              high=donation['2020']['high'],
-                                              interval=CREDIBLE_INTERVAL)
-        else:
-            y2020_donation = 0
-
         if random.random() <= donation['2021']['prob']:
             y2021_donation = lognormal_sample(low=donation['2021']['low'],
                                               high=donation['2021']['high'],
@@ -162,20 +155,27 @@ for s in range(N_SCENARIOS):
         else:
             y2021_donation = 0
 
-        y2020_donation = round_to_nearest(y2020_donation)
+        if random.random() <= donation['2022']['prob']:
+            y2022_donation = lognormal_sample(low=donation['2022']['low'],
+                                              high=donation['2022']['high'],
+                                              interval=CREDIBLE_INTERVAL)
+        else:
+            y2022_donation = 0
+
         y2021_donation = round_to_nearest(y2021_donation)
+        y2022_donation = round_to_nearest(y2022_donation)
 
         if s % update_every == 0 and VERBOSE:
-            print('{} gives {} in 2020 and {} in 2021'.format(donor,
-                                                              print_money(y2020_donation),
-                                                              print_money(y2021_donation)))
-        y2020_donations.append(y2020_donation)
+            print('{} gives {} in 2021 and {} in 2022'.format(donor,
+                                                              print_money(y2021_donation),
+                                                              print_money(y2022_donation)))
         y2021_donations.append(y2021_donation)
-        y2020_fundraising_totals[donor] = np.append(y2020_fundraising_totals[donor], y2020_donation)
+        y2022_donations.append(y2022_donation)
         y2021_fundraising_totals[donor] = np.append(y2021_fundraising_totals[donor], y2021_donation)
-        joint_fundraising_totals[donor] = np.append(joint_fundraising_totals[donor], y2020_donation + y2021_donation)
+        y2022_fundraising_totals[donor] = np.append(y2022_fundraising_totals[donor], y2022_donation)
+        joint_fundraising_totals[donor] = np.append(joint_fundraising_totals[donor], y2021_donation + y2022_donation)
 
-    y2020_total_raised = sum(y2020_donations)
+    y2021_total_raised = sum(y2021_donations)
     if CONSTANT_ERROR_WIDTH > 0:
         scenario_constant_error = normal_sample(low=-CONSTANT_ERROR_WIDTH/2.0 - CONSTANT_ERROR_MEAN,
                                                 high=CONSTANT_ERROR_WIDTH/2.0 - CONSTANT_ERROR_MEAN,
@@ -183,15 +183,15 @@ for s in range(N_SCENARIOS):
     else:
         scenario_constant_error = 0
 
-    y2021_total_raised = sum(y2021_donations) + scenario_constant_error
+    y2022_total_raised = sum(y2022_donations) + scenario_constant_error
 
     if s % update_every == 0 and VERBOSE:
         print('CONSTANT ERROR TERM: {}'.format(print_money(scenario_constant_error)))
-        print('TOTAL RAISED IN 2020: {}'.format(print_money(y2020_total_raised)))
         print('TOTAL RAISED IN 2021: {}'.format(print_money(y2021_total_raised)))
+        print('TOTAL RAISED IN 2022: {}'.format(print_money(y2022_total_raised)))
 
-    y2020_all_scenario_totals.append(y2020_total_raised)
     y2021_all_scenario_totals.append(y2021_total_raised)
+    y2022_all_scenario_totals.append(y2022_total_raised)
     constant_errors.append(scenario_constant_error)
 
 if VERBOSE:
@@ -200,28 +200,28 @@ if VERBOSE:
     print('-')
 
 
-joint_scenarios = np.array(y2020_all_scenario_totals) + np.array(y2021_all_scenario_totals)
-y2020_percentiles = np.percentile(y2020_all_scenario_totals, SCENARIO_RANGES)
+joint_scenarios = np.array(y2021_all_scenario_totals) + np.array(y2022_all_scenario_totals)
 y2021_percentiles = np.percentile(y2021_all_scenario_totals, SCENARIO_RANGES)
+y2022_percentiles = np.percentile(y2022_all_scenario_totals, SCENARIO_RANGES)
 joint_percentiles = np.percentile(joint_scenarios, SCENARIO_RANGES)
 
-print('SCENARIO 2020 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(y2020_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
 print('SCENARIO 2021 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(y2021_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
-print('SCENARIO 2020+2021 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(joint_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
+print('SCENARIO 2022 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(y2022_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
+print('SCENARIO 2021+2022 -- {}'.format(' -- '.join(['{}%: {}'.format(s, print_money(joint_percentiles[i])) for i, s in enumerate(SCENARIO_RANGES)])))
 
 if SAVE:
     print('... Saving 1/8')
     pickle.dump(fundraising_data, open('{}fundraising_data.p'.format(PATH), 'wb'))
     print('... Saving 2/8')
-    pickle.dump(y2020_fundraising_totals, open('{}y2020_fundraising_totals.p'.format(PATH), 'wb'))
-    print('... Saving 3/8')
     pickle.dump(y2021_fundraising_totals, open('{}y2021_fundraising_totals.p'.format(PATH), 'wb'))
+    print('... Saving 3/8')
+    pickle.dump(y2022_fundraising_totals, open('{}y2022_fundraising_totals.p'.format(PATH), 'wb'))
     print('... Saving 4/8')
     pickle.dump(joint_fundraising_totals, open('{}joint_fundraising_totals.p'.format(PATH), 'wb'))
     print('... Saving 5/8')
-    pickle.dump(y2020_all_scenario_totals, open('{}y2020_all_scenario_totals.p'.format(PATH), 'wb'))
-    print('... Saving 6/8')
     pickle.dump(y2021_all_scenario_totals, open('{}y2021_all_scenario_totals.p'.format(PATH), 'wb'))
+    print('... Saving 6/8')
+    pickle.dump(y2022_all_scenario_totals, open('{}y2022_all_scenario_totals.p'.format(PATH), 'wb'))
     print('... Saving 7/8')
     pickle.dump(joint_scenarios, open('{}joint_scenarios.p'.format(PATH), 'wb'))
     print('... Saving 8/8')
